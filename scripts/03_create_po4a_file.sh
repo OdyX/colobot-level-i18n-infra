8d07b12088a851ad5ff4b7a0c61a54f870b199d9
Didier Raboud <odyx@debian.org>
2013-01-10 08:56:47 +0100

    Add help files and translation infrastructure

--- scripts/03_create_po4a_file.sh
+++ scripts/03_create_po4a_file.sh
@@ -2,9 +2,27 @@
 
 set -e
 
-echo "[po_directory] $1"
-shift 1
+# MUST be run from ${CMAKE_CURRENT_BINARY_DIR}
 
-for scene in "$@"; do
-	echo "[type:xml] $scene.xml \$lang:$scene.\$lang.xml"
+srcdir=$1 # absolute
+po_directory=$2 # relative
+leveldir=$3 # relative
+helpdir=$4 # relative
+
+ln -sf $srcdir/$po_directory $po_directory
+echo "[po_directory] $po_directory"
+
+# Levels are precompiled, they are already in the current dir
+for scene in $(cd $srcdir/$leveldir; ls *.txt); do
+	scene_=$(basename $scene .txt)
+	echo "[type:xml] $scene_.xml \$lang:$scene_.\$lang.xml"
+done
+
+# Create symlink for relative paths in po4a
+mkdir -p $helpdir
+
+for helpfile in $(cd $srcdir/$helpdir; ls *.txt); do
+	helpfile_=$(basename $helpfile .txt)
+	$(cd $helpdir; ln -sf $srcdir/$helpdir/$helpfile $helpfile)
+	echo "[type:text] $helpdir/$helpfile \$lang:$helpdir/$helpfile_.\$lang.txt"
 done
